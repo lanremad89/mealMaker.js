e4baaa990fca4ca8ce2fc4b26340c51bd4dbc78c
lanremad89 <101396651+lanremad89@users.noreply.github.com>
2023-03-29 03:11:29 -0500

    Add files via upload

---
+++ mealMaker.js
@@ -0,0 +1,25 @@
+const menu = {
+    _meal: '',
+    _price: 0,
+    set meal(mealToCheck) {
+        if(typeof mealToCheck === 'string') {
+            return this._meal = mealToCheck;
+        }    
+    },
+    set price(priceToCheck) {
+        if(typeof priceToCheck === 'number') {
+            return this._price = priceToCheck;
+        }
+    },
+    get todaysSpecial() {
+      if(this._meal && this._price) {
+        return `Today’s Special is ${this._meal} for $${this._price}!`;
+      } else {
+          return `Meal or price was not set correctly!`;
+      }
+    }
+  }
+  menu.meal = 'Lasanga';
+  menu.price = 28;
+  console.log(menu);
+  console.log(menu.todaysSpecial);
